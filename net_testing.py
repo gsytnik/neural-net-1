82727930eb03d1d87dce104d62b29a21361e3b5a
Gennady Sytnik <gsytnik@calpoly.edu>
2021-06-14 10:21:53 -0700

    learns but incorrectly. Biases not implemented

--- net_testing.py
+++ net_testing.py
@@ -1,11 +1,50 @@
 from neural_net import Neural_Net
 import numpy as np
+from PIL import Image
+from skimage.transform import resize
+import random
+import glob
 
 def main():
 	
-	network = Neural_Net(9, 10, 10, 9)
-	network.obtain_inputs([1, -1, 1, 0, 0, 0, -1, 1, 1])
-	network.train()
+	network = Neural_Net(720000, 4, 80, 1, 0.1)
+	
+	files = []
+	for file in glob.glob('C:\\Users\\gsytn\\Documents\\SchoolAndCS\\ML\\neural-net-1\\test_data\\dataset\\training_set\\dogs\\*.jpg'):
+		files.append(file)
+	
+	for file in glob.glob('C:\\Users\\gsytn\\Documents\\SchoolAndCS\\ML\\neural-net-1\\test_data\\dataset\\training_set\\cats\\*.jpg'):
+		files.append(file)
+
+	random.shuffle(files)
+	
+	network.load_training()
+
+	for file in files:
+		if 'cat' in file:
+			expected = [0]
+		else:
+			expected = [1]
+
+		img = Image.open(file)
+		img = np.array(img)
+		img = resize(img, (400, 600)).ravel()
+		network.train(img, expected)
+		if network.count > 200:
+			break
+	
+	network.save_training()
+		
+	for file in glob.glob('C:\\Users\\gsytn\\Documents\\SchoolAndCS\\ML\\neural-net-1\\test_data\\dataset\\training_set\\dog\\*.jpg'):
+		print("e")
+		expected = [1]
+		img = Image.open(file)
+		img = np.array(img)
+		img = resize(img, (400, 600)).ravel()
+		network.run(img)
+
+	
+
 
 if __name__ == '__main__':
     main()
--- neural_net.py
+++ neural_net.py
@@ -2,21 +2,23 @@ import numpy as np
 import math
 import random
 
-beta = 1.5
+beta = 1.125
 np.set_printoptions(precision=5)
 
 class Neural_Net:
 
 	# create a neural network which is generated via a specific number of inputs, 
 	# a specific number of layers, and a number of outputs. 
-	def __init__(self, num_inputs, num_layers, layer_size, num_outputs):
+	def __init__(self, num_inputs, num_layers, layer_size, num_outputs, learning_rate):
 		print("creating network with:\n\n\t{0} inputs,\n\t{1} layers of size {2},\n\t{3} outputs.".format(
 			num_inputs, num_layers, layer_size, num_outputs)
 		)
+		self.lr = learning_rate
 		self.num_layers = num_layers
 		self.num_inputs = num_inputs
 		self.layer_size = layer_size
 		self.num_outputs = num_outputs
+		self.count = 0
 		self.init_weights()
 		self.init_biases()
 		self.init_neurons()
@@ -28,15 +30,15 @@ class Neural_Net:
 		self.weights = [] 
 
 		# create first set of weights from input layer to first hidden layer
-		self.weights.append(np.random.uniform(low= -1.5, high = 1.5, size=(self.layer_size, self.num_inputs)))
+		self.weights.append(np.random.uniform(low = -0.5, high = 0.5, size=(self.layer_size, self.num_inputs)))
 		# for each hidden layer
 		for layer in range(self.num_layers):
 			# if it is the last layer, append weight matrix size (num_outputs, layer_size)
 			if layer == self.num_layers - 1:
-				self.weights.append(np.random.uniform(low= -1.5, high = 1.5, size=(self.num_outputs, self.layer_size)))
+				self.weights.append(np.random.uniform(low = -1, high = 1, size=(self.num_outputs, self.layer_size)))
 			# else, append weight matrix size (layer_size, layer_size)
 			else:
-				self.weights.append(np.random.uniform(low= -1.5, high= 1.5, size=(self.layer_size, self.layer_size)))
+				self.weights.append(np.random.uniform(low = -1, high = 1, size=(self.layer_size, self.layer_size)))
 
 	# init biases to add to each result.
 	def init_biases(self):
@@ -45,10 +47,10 @@ class Neural_Net:
 		for i in range(self.num_layers + 1):
 			# if output layer, append array of size num_outputs with biases between -5, 5
 			if i == self.num_layers:
-				self.biases.append(np.random.uniform(low = -5.0, high = 5.0, size = self.num_outputs))
+				self.biases.append(np.random.uniform(low = -5, high = 5, size = self.num_outputs))
 			# else, append array of size layer_size with biases between -5, 5
 			else:
-				self.biases.append(np.random.uniform(low = -5.0, high = 5.0, size = self.layer_size))
+				self.biases.append(np.random.uniform(low = -5, high = 5, size = self.layer_size))
 
 	# init neurons.
 	def init_neurons(self):
@@ -63,25 +65,82 @@ class Neural_Net:
 				self.neurons.append(np.zeros(self.layer_size))
 
 	# take in input array and convert to np array.
-	def obtain_inputs(self, inputs):
-		self.current_inputs = np.asarray(inputs)
+	def obtain_input(self, inp):
+		self.current_inputs = np.asarray(inp)
 
 	# basic feed forward algorithm to generate some output.
 	# first take inputs, multiply by weight matrix, 
 	# then add biases to result, 
 	# then run through activation function.
 	def feed_forward(self, inputs, layer):	
-		self.neurons[layer] = np.matmul(self.weights[layer], inputs) + self.biases[layer]
+		self.neurons[layer] = np.matmul(self.weights[layer], inputs, dtype='float64') + self.biases[layer]
 		self.neurons[layer] = np.array(list(map(activ_function, self.neurons[layer])))
 
 		if layer < self.num_layers:
 			self.feed_forward(self.neurons[layer], layer + 1)
 
-	# function to train this algorithm
-	def train(self):
+	def run(self, inp):
+		self.obtain_input(inp)
 		self.feed_forward(self.current_inputs, 0)
-		print(self.current_inputs, self.neurons[-1])
+		if (self.neurons[-1][0] >= 0.5):
+			print("dog.")
+		else:
+			print("not a dog.")
 
+		print(self.neurons[-1][0])
+
+
+	# function to train this network
+	def train(self, inp, expected):
+		self.obtain_input(inp)
+		self.feed_forward(self.current_inputs, 0)
+
+		self.backprop(expected, 1)
+		self.count += 1
+		print("loop {0}".format(self.count))
+		
+
+
+	def backprop(self, expected, layer):
+
+		layer_errors = np.matmul(np.transpose(self.weights[-layer]), (expected - self.neurons[-layer]))
+		
+
+		'''
+		dM = lr * errors * deriv of sigm * transposed inputs
+		dB = lr * errors
+		'''
+
+		# d_b = self.lr * layer_errors
+		# self.biases[-layer] += d_b
+
+		if layer < self.num_layers + 1:
+			d_m = self.delta_m(layer, layer_errors, self.neurons[layer-1])
+		else:
+			d_m = self.delta_m(layer, layer_errors, self.current_inputs)
+
+		self.weights[-layer] += d_m
+		
+		# loop over all layers.
+		if layer < self.num_layers + 1:
+			self.backprop(self.neurons[-layer - 1] + layer_errors, layer + 1)
+		
+	def delta_m(self, layer, layer_errors, inputs):
+		
+		'''
+		gradient = vector product of: 
+		(
+		resulting neurons mapped through activation derivative,
+		errors of the neurons in current layer.
+		)
+		'''
+		gradient = self.lr * (
+			np.array(list(map(activ_deriv, self.neurons[-layer]))).reshape(-1,1)
+			@
+			layer_errors.reshape(1,-1) 
+		)
+
+		return np.transpose(inputs) * gradient
 			
 	def print_weights(self, layer):
 		print(self.weights[layer])
@@ -98,6 +157,15 @@ class Neural_Net:
 			print("layer {0}:\n".format(layer - 1))
 			self.print_weights(layer)
 
+	def save_training(self):
+		np.save("weights", self.weights)
+		np.save("biases", self.biases)
+
+	def load_training(self):
+		self.weights = [arr for arr in np.load("weights.npy", allow_pickle=True)]
+		self.biases = [arr for arr in np.load("biases.npy", allow_pickle=True)]
+
+
 def sigmoid(x):
 	if x >= 0 :
 		z =  np.exp(-x)
@@ -107,7 +175,10 @@ def sigmoid(x):
 		return z / (1 + z)
 
 def activ_function(x):
-	return beta * x * sigmoid(x)
+	return sigmoid(x)
 
-def activ_deriv(x):
-	return activ_function(x) + (sigmoid(x) * (beta - activ_function(x)))
+def activ_deriv(y):
+	return  y * (1 - y)
+
+def precis(x):
+		return math.ceil(x*1e10)/1e10
